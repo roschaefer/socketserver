4b0bf73a5a69bf673f490eee3a636d26f743fae1
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-09 18:27:28 +0200

    use forever for deployment

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,5 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-cd /home/pi/StoryTrolley/server
-pwd
-pkill node && npm install && node server.js --client ../client/dist &
-pwd
-
+forever-restart
 DISPLAY=:0 chromium-browser 'http://localhost:8000' --start-fullscreen
